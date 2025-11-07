803c6d90555dd742351ff3e5e776ef8ae4b5cf61
Nicolasgharzouzi <nicolasgharzouzi04@gmail.com>
2025-11-07 10:32:23 -0500

    FNode update

--- FileServer/src/main/java/ca/concordia/filesystem/datastructures/FNode.java
+++ FileServer/src/main/java/ca/concordia/filesystem/datastructures/FNode.java
@@ -1,12 +1,16 @@
 package ca.concordia.filesystem.datastructures;
 
 public class FNode {
+    
+    private int blockIndex; 
+    private int next;       
 
-    private int blockIndex;
-    private int next;
+    public FNode() { this.blockIndex = -1; this.next = -1; }
 
-    public FNode(int blockIndex) {
-        this.blockIndex = blockIndex;
-        this.next = -1;
-    }
+    public int getBlockIndex() { return blockIndex; }
+    public void setBlockIndex(int blockIndex) { this.blockIndex = blockIndex; }
+    public int getNext() { return next; }
+    public void setNext(int next) { this.next = next; }
+
+    public boolean isFree() { return blockIndex < 0; }
 }
