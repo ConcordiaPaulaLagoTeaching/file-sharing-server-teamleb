a13e99bca952c6a4a05dcfac024127f49475d73e
Nicolasgharzouzi <nicolasgharzouzi04@gmail.com>
2025-11-13 22:16:23 -0500

    Added comments

--- FileServer/src/main/java/ca/concordia/Main.java
+++ FileServer/src/main/java/ca/concordia/Main.java
@@ -6,16 +6,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
 
+        // Basic filesystem parameters
         int blockSize = 128;
         int maxFiles  = 5000;
         int maxBlocks = 2000;
 
-      
+        // Compute how big the backing file needs to be
         int metaBytes   = maxFiles * 16 + maxBlocks * 8;
         int metaBlocks  = (metaBytes + blockSize - 1) / blockSize;
-        int totalBlocks = metaBlocks + maxBlocks + 16;
+        int totalBlocks = metaBlocks + maxBlocks + 16; 
         int totalSize   = totalBlocks * blockSize;
 
+        // Create and start the file server on port 12345
         FileServer server = new FileServer(
                 12345,
                 "filesystem.dat",
