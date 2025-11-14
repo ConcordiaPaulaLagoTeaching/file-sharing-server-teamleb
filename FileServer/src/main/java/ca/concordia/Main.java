b3da5d93e32e66347517c76be5353a64ebd29d89
Nicolasgharzouzi <nicolasgharzouzi04@gmail.com>
2025-11-13 21:17:40 -0500

    Main

--- FileServer/src/main/java/ca/concordia/Main.java
+++ FileServer/src/main/java/ca/concordia/Main.java
@@ -4,10 +4,23 @@ import ca.concordia.server.FileServer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
-        FileServer server = new FileServer(12345, "filesystem.dat", 10 * 128);
-        // Start the file server
+        int blockSize = 128;
+        int maxFiles  = 5000;
+        int maxBlocks = 2000;
+
+      
+        int metaBytes   = maxFiles * 16 + maxBlocks * 8;
+        int metaBlocks  = (metaBytes + blockSize - 1) / blockSize;
+        int totalBlocks = metaBlocks + maxBlocks + 16;
+        int totalSize   = totalBlocks * blockSize;
+
+        FileServer server = new FileServer(
+                12345,
+                "filesystem.dat",
+                blockSize, maxFiles, maxBlocks, totalSize
+        );
         server.start();
     }
 }
